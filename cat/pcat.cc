0d5e42f61d5a88b150ea0863fdb496782b892144
Instigo <emanon4ever@gmail.com>
2014-09-09 17:41:37 -0400

    initial version

--- cat/pcat.cc
+++ cat/pcat.cc
@@ -1,48 +1,8 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/param.h>
-#include <sys/time.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <grp.h>
-#include <time.h>
-#include <locale.h>
 #include <string.h>
 
 #define MAX_FILENAME_CHARS 255
-
-//
-// Name: getFileType
-//
-// Description: Returns the filetype of a file
-/*const char *getPermissions(mode_t mode){
-
-    char filetype = '?';
-
-    if(S_ISREG(mode)) filetype = '-';
-    if(S_ISLNK(mode)) filetype = 'l';
-    if(S_ISDIR(mode)) filetype = 'd';
-    if(S_ISBLK(mode)) filetype = 'b';
-    if(S_ISCHR(mode)) filetype = 'c';
-    if(S_ISFIFO(mode)) filetype = '|';
-
-
-    return (const char*)permBuffer;
-}*/
-
-
-// 
-// Name: kill
-//
-// Description: Kills the program with a given error message
-//
-void kill(char *message){
-
-    perror(message);
-    exit(0);
-}
 
 int main(int argc, char * argv[]){
 
